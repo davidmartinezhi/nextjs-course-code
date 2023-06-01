1be7191da955c09acf58749a6976cb6eeb8465b6
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-06-01 13:33:54 -0600

    adapting to starting project

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
-import Head from 'next/head';
-
-import '../styles/globals.css';
-import Layout from '../components/layout/layout';
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    // <Head>
+    //   <meta name='viewport' content='width=device-width, initial-scale=1' />
+    // </Head>
+    <Component {...pageProps} />
   );
 }
 
