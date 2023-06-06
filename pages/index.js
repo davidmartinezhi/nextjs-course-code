bcc52b211cadac2cb0dcb2737c3224c039f2e20f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-06-06 13:55:10 -0600

    using mardown data to render file

--- pages/index.js
+++ pages/index.js
@@ -2,49 +2,26 @@ import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
-
-const DUMMY_POSTS = [
-  {
-    title: "Getting Started With Nextjs",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs",
-  },
-  {
-    title: "Getting Started With Nextjs1",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR1",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs1",
-  },
-  {
-    title: "Getting Started With Nextjs2",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR2",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs2",
-  },
-  {
-    title: "Getting Started With Nextjs3",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR3",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs3",
-  },
-];
+import { getFeaturedPosts } from "../lib/posts-util";
 
 function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts(); //get all featured posts
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 100,
+  };
 }
 
 export default HomePage;
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -1,6 +1,6 @@
 ---
 title: "Getting started with nextJs"
-date: "2022-10-15"
+date: "2022-10-16"
 image: "getting-started-nextjs.png"
 excerpt: "Next Js is a react framework for production, ships with built in SSR"
 isFeatured: true
