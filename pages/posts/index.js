15cb64fc5fe978881b45cecc43cb5557e6f8df0f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-06-06 13:59:05 -0600

    rendering markDown data for all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,42 +1,21 @@
 import AllPosts from "../../components/posts/all-posts";
-
-const DUMMY_POSTS = [
-  {
-    title: "Getting Started With Nextjs",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs",
-  },
-  {
-    title: "Getting Started With Nextjs1",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR1",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs1",
-  },
-  {
-    title: "Getting Started With Nextjs2",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR2",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs2",
-  },
-  {
-    title: "Getting Started With Nextjs3",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Next Js is a react framework for production, ships with built in SSR3",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs3",
-  },
-];
+import { getAllPosts } from "../../lib/posts-util";
 
 function AllPostsPage(props) {
-  return <AllPosts posts={DUMMY_POSTS} />;
+  return <AllPosts posts={props.posts} />;
+}
+
+export function getStaticProps() {
+  //get all posts
+  const allPosts = getAllPosts();
+
+  //return props
+  return {
+    props: {
+      posts: allPosts,
+    },
+    revalidate: 100,
+  };
 }
 
 export default AllPostsPage;
