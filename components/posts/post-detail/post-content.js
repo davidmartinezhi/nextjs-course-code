7408aa45c05bb49520e2d8aad83836fa93610113
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-06-17 03:36:16 -0600

    rendering images with next from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,25 +1,54 @@
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 
-const DUMMY_POST = {
-  title: "Getting Started With Nextjs",
-  image: "getting-started-nextjs.png",
-  excerpt:
-    "Next Js is a react framework for production, ships with built in SSR",
-  date: "2022-02-10",
-  slug: "getting-started-with-nextjs",
-  content: "# This is a first post",
-};
+import Image from "next/image";
 
 import classes from "./post-content.module.css";
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  console.log("==========");
+  console.log(post);
+
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customRenderers = {
+    /*
+    image(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+    */
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,36 @@
 // "/posts/next-js-course" <- it is a slug if it is human readable
 
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostsData, getPostsFiles } from "../../lib/posts-util";
 
 function PostDetailPage(props) {
-  return <PostContent />;
+  return <PostContent post={props.post} />;
 }
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostsData(slug);
+  //console.log(postData);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
+
 export default PostDetailPage;
