f4d6836237b95c7d9ea8e597a307ebf98116e0a3
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-07-07 21:17:38 -0600

    rendering code snippets from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import Image from "next/image";
 
@@ -42,6 +44,17 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { language, value } = code;
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={value}
+        ></SyntaxHighlighter>
+      );
     },
   };
 
